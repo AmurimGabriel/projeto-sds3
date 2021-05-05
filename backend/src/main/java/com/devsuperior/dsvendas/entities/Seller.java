eb96412c849964b73d7bef64fba34ad5fe588ac0
Gabriel Souza <amurim.souza27@gmail.com>
2021-05-05 20:13:56 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -3,7 +3,6 @@ package com.devsuperior.dsvendas.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,8 +17,6 @@ public class Seller {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	@Column(nullable = false)
 	private String name;
 
 	@OneToMany(mappedBy = "seller")
