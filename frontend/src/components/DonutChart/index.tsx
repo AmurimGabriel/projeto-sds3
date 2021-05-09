457e5ae8e88dd5c26e16691681c0a5a525a7b3db
Gabriel Souza <amurim.souza27@gmail.com>
2021-05-09 20:16:35 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,7 +11,7 @@ type ChartData = {
 
 function DonutChart() {
 
-    const [chartData, setChatData] = useState<ChartData>({ labels: [], series: [] });
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
@@ -20,7 +20,7 @@ function DonutChart() {
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
-                setChatData({ labels: myLabels, series: mySeries });
+                setChartData({ labels: myLabels, series: mySeries });
             });
     },
         []
